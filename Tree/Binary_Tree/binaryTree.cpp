82df2de98b40f9003f9775e5ff1763809a39d464
coderharsx1122 <harshit.tripathihero15@gmail.com>
2023-04-14 13:27:53 +0530

    modified binaryTree.cpp file

--- Tree/Binary_Tree/binaryTree.cpp
+++ Tree/Binary_Tree/binaryTree.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
-    Node(int data){
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
         this->data = data;
         this->left = NULL;
-        this->right= NULL;
+        this->right = NULL;
     }
 };
 
 // FUNCTION TO BUILD BINARY SEARCH TREE
-Node* buildTree(Node* root){
+Node *buildTree(Node *root)
+{
     int data;
-    cout<<"Enter the data for node"<<endl;
-    cin>>data;
-    if(data == -1) return NULL;
+    cout << "Enter the data for node" << endl;
+    cin >> data;
     root = new Node(data);
+    if (data == -1)
+        return NULL;
 
+    //    insert data for left node
+    cout << "Insert data for left Node of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Insert data for right Node of " << data << endl;
+    root->right = buildTree(root->right);
 
-//    insert data for left node
-   cout<<"Insert data for left Node of"<<data<<endl;
-   root->left = buildTree(root->left);
-   cout<<"Insert data for right Node of"<<data<<endl;
-   root->left = buildTree(root->right);
-
-   return root;
+    return root;
 }
 
-void inOrderTrav(Node* root){
-    if(root == NULL) return;
+void inOrderTrav(Node *root)
+{
+    if (root == NULL){
+        return;
+    }
+
     inOrderTrav(root->left);
-    cout<<root->data<<endl;
+    cout << root->data << " ";
     inOrderTrav(root->right);
+    return;
+}
+void priOrderTrav(Node *root)
+{
+    if (root == NULL){
+        return;
+    }
+
+    cout << root->data << " ";
+    inOrderTrav(root->left);
+    inOrderTrav(root->right);
+    return;
+}
+void postOrderTrav(Node *root)
+{
+    if (root == NULL){
+        return;
+    }
+
+    inOrderTrav(root->left);
+    inOrderTrav(root->right);
+    cout << root->data << " ";
+    return;
 }
 
-int main(){
-   Node* root = NULL;
-   root = buildTree(root);
-   inOrderTrav(root);
+int main()
+{
+    Node *root = NULL;
+    root = buildTree(root);
+    inOrderTrav(root);
 }
